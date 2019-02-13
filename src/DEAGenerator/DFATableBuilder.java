398bb031f1a2a8ba03ef3291676d5cfb18bc9dea
Baserror <barbeeker@gmail.com>
2019-02-14 00:46:14 +0100

    tomisdum

--- src/DEAGenerator/DFATableBuilder.java
+++ src/DEAGenerator/DFATableBuilder.java
@@ -1,4 +1,5 @@
 package DEAGenerator;
+
 import Visitors.*;
 
 import java.util.ArrayList;
@@ -9,36 +10,37 @@ import java.util.TreeMap;
 
 public class DFATableBuilder {
 
-    SortedMap<Integer, FollowposTableEntry> followPosTableEntries= new TreeMap<>();
-    DFAMap theStateMap;
-    public DFATableBuilder(){
-        theStateMap=new DFAMap();
+    SortedMap<Integer, FollowposTableEntry> followPosTableEntries = new TreeMap<>();
+    DFAMap stateMap;
+
+    public DFATableBuilder() {
+        stateMap = new DFAMap();
     }
 
-    public SortedMap<DFAState, Map<Character, DFAState>> createDFATable(SortedMap<Integer, FollowposTableEntry> followPosTableEntries){
-        ArrayList<DFAState> theStates = new ArrayList<>();
-        for(int i=0;i<followPosTableEntries.size();i++){
-            boolean isAcceptingState = followPosTableEntries.get(i).symbol=="#";
-            DFAState tempState = new DFAState(followPosTableEntries.get(i).position,isAcceptingState, followPosTableEntries.get(i).followpos);
-            theStates.add(tempState);
+    public SortedMap<DFAState, Map<Character, DFAState>> createDFATable(SortedMap<Integer, FollowposTableEntry> followPosTableEntries) {
+        ArrayList<DFAState> stateEntries = new ArrayList<>();
+        for (int i = 0; i < followPosTableEntries.size(); i++) {
+            boolean isAcceptingState = followPosTableEntries.get(i).symbol == "#";
+            stateEntries.add(new DFAState(followPosTableEntries.get(i).position,
+                    isAcceptingState,
+                    followPosTableEntries.get(i).followpos));
         }
-        SortedMap<DFAState, Map<Character, DFAState>> stateTransitionTable = new TreeMap<>();
-        for(int i=0;i<theStates.size();i++){
-            Map<Character,DFAState> tempMap=new TreeMap<>();
-            Object[] theFollowPositions = theStates.get(i).positionsSet.toArray();
-            for(int j=0;j<theFollowPositions.length;j++){
-                for(int k=0;k<followPosTableEntries.size();k++){
-                    if((int)theFollowPositions[j]== followPosTableEntries.get(k).position){
-                        String theString=followPosTableEntries.get(k).symbol;
-                        tempMap.put(theString.charAt(0),theStates.get(k));
+        SortedMap<DFAState, Map<Character, DFAState>> dfaTransitionMatrix = new TreeMap<>();
+        for (int i = 0; i < stateEntries.size(); i++) {
+            Map<Character, DFAState> temporaryMap = new TreeMap<>();
+            Object[] theFollowPositions = stateEntries.get(i).positionsSet.toArray();
+            for (int j = 0; j < theFollowPositions.length; j++) {
+                for (int k = 0; k < followPosTableEntries.size(); k++) {
+                    if ((int) theFollowPositions[j] == followPosTableEntries.get(k).position) {
+                        String theString = followPosTableEntries.get(k).symbol;
+                        temporaryMap.put(theString.charAt(0), stateEntries.get(k));
                     }
                 }
             }
-            stateTransitionTable.put(theStates.get(i),tempMap);
+            dfaTransitionMatrix.put(stateEntries.get(i), temporaryMap);
         }
-        return stateTransitionTable;
+        return dfaTransitionMatrix;
     }
-
 
 
 }
