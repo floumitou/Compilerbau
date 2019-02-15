1f2720d0919c025d5b95007e28bd3e131292681f
Tom Scholl <Tom.scholl@Kaufland.com>
2019-02-15 19:54:46 +0100

    für die fans

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,14 @@
 import TopDownParser.TopDownParser;
-import Visitors.IVisitable;
+import Visitors.*;
 
 import java.util.Scanner;
+import java.util.SortedMap;
 
 public class Main {
 
     public static void main(String[] args) {
+
+        //Diego
         Scanner input = new Scanner(System.in);
         System.out.println("Please enter a regular Expression:");
         String regExp = input.next();
@@ -13,7 +16,21 @@ public class Main {
 
         IVisitable SyntaxTree = parser.start();
 
+
+        //Florian
         FirstVisitor firstVisitor = new FirstVisitor();
         SyntaxTree = firstVisitor.visitTreeNodes(SyntaxTree);
+
+
+
+        //Tom
+        SortedMap<Integer, FollowposTableEntry> followPosTableEntries;
+
+        SecondVisitor secondVisitor = new SecondVisitor();
+        DepthFirstIterator.traverse(SyntaxTree, secondVisitor);
+        followPosTableEntries = secondVisitor.getFollowposTableEntries();
+
+
+        //Basti macht Sachen mit FollowPosTableEntries
     }
 }
